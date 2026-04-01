1ae0d22cdd1c14a94abb4a31a64481eafb455ea2
Liya  Mehamed <liyam8376@gmail.com>
2026-04-02 00:05:39 +0300

    Update SectionWrapper.jsx

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -6,7 +6,7 @@ export default function SectionWrapper({
   className = '', 
   backgroundColor = 'white' 
 }) {
-  const bgColor = backgroundColor === 'cloud' ? 'var(--cloud-break)' : 'white';
+  const bgColor = backgroundColor === 'cloud' ? 'var(--cloud-break)' : backgroundColor;
   
   return (
     <section 
